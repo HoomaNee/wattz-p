8a8f4a760ea2fa62b11c96d40f57ae401127c998
HoomaNee <hell.s.o.s.no.body@gmail.com>
2025-09-08 22:26:31 +0330

    Fix SettingsActivity.kt

--- app/src/main/java/dubrowgn/wattz/SettingsActivity.kt
+++ app/src/main/java/dubrowgn/wattz/SettingsActivity.kt
@@ -1,4 +1,3 @@
-// Modified SettingsActivity.kt
 package dubrowgn.wattz
 
 import android.annotation.SuppressLint
@@ -8,11 +7,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import android.widget.EditText // Added: Import for EditText
 import android.widget.RadioGroup
 import android.widget.Switch
 import android.widget.TextView
@@ -29,10 +25,6 @@ class SettingsActivity : Activity() {
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     private lateinit var invertCurrent: Switch
     private lateinit var power: TextView
-
-    // Added: EditTexts for notification colors (assume added to activity_settings.xml with ids notification_background_color and notification_text_color)
-    private lateinit var notificationBackgroundColor: EditText
-    private lateinit var notificationTextColor: EditText
 
     private fun debug(msg: String) {
         Log.d(this::class.java.name, msg)
@@ -73,10 +65,6 @@ class SettingsActivity : Activity() {
             }
         )
         invertCurrent.isChecked = settings.getBoolean("invertCurrent", false)
-
-        // Added: Load notification color preferences
-        notificationBackgroundColor.setText(settings.getString("notificationBackgroundColor", "#FFFFFF"))
-        notificationTextColor.setText(settings.getString("notificationTextColor", "#000000"))
     }
 
     @SuppressLint("ApplySharedPref")
@@ -104,9 +92,6 @@ class SettingsActivity : Activity() {
                     else -> "W"
                 }
             )
-            // Added: Save notification colors
-            .putString("notificationBackgroundColor", notificationBackgroundColor.text.toString())
-            .putString("notificationTextColor", notificationTextColor.text.toString())
             .commit()
 
         sendBroadcast(Intent().setPackage(packageName).setAction(settingsUpdateInd))
@@ -125,27 +110,11 @@ class SettingsActivity : Activity() {
         invertCurrent = findViewById(R.id.invertCurrent)
         power = findViewById(R.id.power)
 
-        // Added: Initialize EditTexts for colors
-        notificationBackgroundColor = findViewById(R.id.notification_background_color)
-        notificationTextColor = findViewById(R.id.notification_text_color)
-
         loadPrefs()
 
         currentScalar.setOnCheckedChangeListener { _, _ -> onChange() }
         indicatorUnits.checkChangedCallback = { _ -> onChange() }
         invertCurrent.setOnCheckedChangeListener { _, _ -> onChange() }
-
-        // Added: TextWatchers to trigger onChange when color inputs change
-        notificationBackgroundColor.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) { onChange() }
-        })
-        notificationTextColor.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) { onChange() }
-        })
     }
 
     override fun onPause() {
